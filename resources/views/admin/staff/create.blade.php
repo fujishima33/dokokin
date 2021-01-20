5608a2a7a96fbb1a1e2c1109ae6f95661088e0f1
fujishima <ko.fujishima8@gmail.com>
2021-01-20 07:45:30 +0000

    テンプレート修正

--- resources/views/admin/staff/create.blade.php
+++ resources/views/admin/staff/create.blade.php
@@ -3,14 +3,14 @@
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', '社員新規登録')
+@section('title', '新規登録')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>社員新規登録</h2>
+                <h2>新規登録</h2>
             </div>
         </div>
     </div>
