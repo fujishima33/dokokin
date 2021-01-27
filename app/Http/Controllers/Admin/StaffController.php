a59d39453ca0a9b3e12d10b1f506ee29845b8e79
fujishima <ko.fujishima8@gmail.com>
2021-01-27 14:09:10 +0000

    1.5社員情報の編集と削除

--- app/Http/Controllers/Admin/StaffController.php
+++ app/Http/Controllers/Admin/StaffController.php
@@ -14,22 +14,9 @@ class StaffController extends Controller
       return view('admin');
   }
   
-  //   public function show()
-  // {
-  //     return view('admin.staff');
-  // }
-  
     public function add()
   {
       return view('admin.staff.create');
-  }
-  
-  public function edit(Request $request)
-  {
-      // News Modelからデータを取得する
-      //$news = News::find($request->id);
-      //return view('admin.news.edit', ['news_form' => $news]);
-      return view('admin.staff.edit');
   }
   
   public function create(Request $request)
@@ -52,4 +39,35 @@ class StaffController extends Controller
       return view('admin.staff', ['posts' => $posts]);
   }
   
+  public function edit(Request $request)
+  {
+      $user = User::find($request->id);
+      if (empty($user)) {
+        abort(404);
+      }
+      return view('admin.staff.edit', ['user_form' => $user]);
+  }
+  
+  public function update(Request $request)
+  {
+      //User Modelからデータを取得
+      $user = User::find($request->id);
+      
+      // 送信されてきたフォームデータを格納する
+      $user_form = $request->all();
+      
+      unset($user_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $user->fill($user_form)->save();
+      
+      return redirect('admin/staff');
+  }
+  
+  public function delete(Request $request)
+  {
+      $user = User::find($request->id);
+      $user->delete();
+      return redirect('admin/staff');
+  }  
 }
